5282cfe832f2cd6bee860a03f1724328f79fa08f
DanyloAndrieiev <danssg08@gmail.com>
2026-03-05 16:23:14 +0100

    WIP

--- src/main.cu
+++ src/main.cu
@@ -4,6 +4,15 @@
 using uint = uint32_t;
 
 namespace Kernels {
+    template<class T>
+    __global__ void MaxReduce(T* input) {
+        T value = input[threadIdx.x];
+        for (uint i = 16; i >= 1; i /= 2) {
+            value = max(__shfl_xor_sync(0xffffffff, value, i, 32), value);
+        }
+        printf("Thread %d final value = %d\n", threadIdx.x, value);
+    }
+
     __global__ void Conv2D(const float* input, uint sizeX, uint sizeY, uint stride, const float* filter, float* output) {
         uint2 dtID = {blockIdx.x * (blockDim.x + stride) + threadIdx.x, blockIdx.y * (blockDim.y + stride) + threadIdx.y};
         if (dtID.x > sizeX && dtID.y < sizeY)
@@ -13,17 +22,13 @@ namespace Kernels {
         atomicAdd(&output[blockIdx.y * gridDim.x + blockIdx.x], weighted);
     }
 
-    __global__ void BatchNorm2d() {
-
-    }
-
     __global__ void MaxPool2D(const float* input, uint sizeX, uint sizeY, uint stride, float* output) {
         uint2 dtID = {blockIdx.x * (blockDim.x + stride) + threadIdx.x, blockIdx.y * (blockDim.y + stride) + threadIdx.y};
         if (dtID.x > sizeX && dtID.y < sizeY)
             return;
         const uint filterSizeX = blockDim.x;
         float weighted = input[dtID.y * sizeX + dtID.x];
-        atomicMax(&output[blockIdx.y * gridDim.x + blockIdx.x], weighted);
+        // atomicMax(&output[blockIdx.y * gridDim.x + blockIdx.x], weighted);
     }
 }
 
@@ -38,33 +43,32 @@ int main() {
     }
     printf("Loaded image with a width of %dpx, a height of %dpx and %d channels\n", width, height, channels);
 
-    for (size_t y = 0; y < height; y += 4) {
-        for (size_t x = 0; x < width; x += 2) {
-            unsigned char r = img[(y * width + x) * channels + 0];
-            unsigned char g = img[(y * width + x) * channels + 1];
-            unsigned char b = img[(y * width + x) * channels + 2];
-
-            float v = static_cast<float>(r) / 255.f;
-            if (v == 0) {
-                printf(" ");
-            } else if (v < 0.25f) {
-                printf("░");
-            } else if (v > 0.25f && v < 0.5f) {
-                printf("▒");
-            } else if (v > 0.5f && v < 0.75f) {
-                printf("▓");
-            } else {
-                printf("█");
-            }
-        }
-        printf("\n");
-    }
+    // for (size_t y = 0; y < height; y += 4) {
+    //     for (size_t x = 0; x < width; x += 2) {
+    //         unsigned char r = img[(y * width + x) * channels + 0];
+    //         unsigned char g = img[(y * width + x) * channels + 1];
+    //         unsigned char b = img[(y * width + x) * channels + 2];
+    //
+    //         float v = static_cast<float>(r) / 255.f;
+    //         if (v == 0) {
+    //             printf(" ");
+    //         } else if (v < 0.25f) {
+    //             printf("░");
+    //         } else if (v > 0.25f && v < 0.5f) {
+    //             printf("▒");
+    //         } else if (v > 0.5f && v < 0.75f) {
+    //             printf("▓");
+    //         } else {
+    //             printf("█");
+    //         }
+    //     }
+    //     printf("\n");
+    // }
 
     float* dImage = nullptr;
     cudaMalloc(&dImage, width * height * channels * sizeof(float));
 
-
-
+    Kernels::MinReduce<<<1, 32>>>();
 
     return 0;
 }
