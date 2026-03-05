c1ffd6241b5c46c43316c8574263f25a8934ac86
DanyloAndrieiev <danssg08@gmail.com>
2026-03-05 16:29:08 +0100

    WIP

--- src/pch.cuh
+++ src/pch.cuh
@@ -2,3 +2,15 @@
 
 #include <iostream>
 #include <cassert>
+
+template <class T, std::enable_if_t<std::is_integral_v<T>, int> = 0>
+constexpr auto CeilToMultipleOf(T intValue, T multiple) noexcept
+{
+    return ((intValue + multiple - 1) / multiple) * multiple;
+}
+
+template <class T, std::enable_if_t<std::is_integral_v<T>, int> = 0>
+constexpr auto IntDivideCeil(T intValue, T factor) noexcept
+{
+    return (intValue + factor - 1) / factor;
+}
